a0102a9af783f1492b5e35e5627507e0ad969b48
MD Raza <raza231198@gmail.com>
2020-02-03 19:34:41 +0530

    msm8937-common: build QTI camera device HAL & DisplayService

--- msm8937.mk
+++ msm8937.mk
@@ -85,7 +85,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    android.frameworks.displayservice@1.0
 
 # CNE
 PRODUCT_PACKAGES += \
